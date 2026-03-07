34d96172fa8d137da79d46775ee1a91637836127
Wallison Deyvid Durães Guimarães <wallisondeyvid13@gmail.com>
2026-03-06 23:12:45 -0300

    fix(gestor): remove fachada de UnitProvisioningService em services

--- src/modules/gestor/app/services/UnitProvisioningService.js
+++
@@ -1,1 +0,0 @@
-export * from '#modules/gestor/app/usecases/unit-provisioning/UnitProvisioningService.js';
--- src/modules/gestor/app/services/index.js
+++ src/modules/gestor/app/services/index.js
@@ -1,2 +1,1 @@
 export * from './userService.js';
-export * from './UnitProvisioningService.js';
