50d37c3090796870a0932c38441895052c0ca55b
Wallison Deyvid Durães Guimarães <wallisondeyvid13@gmail.com>
2026-04-19 16:24:11 -0300

    gestor: explicita auth na rota de bancos

--- src/modules/gestor/app/routes/bancoApi.js
+++ src/modules/gestor/app/routes/bancoApi.js
@@ -1,8 +1,9 @@
 // Rota API bancos
 import express from 'express';
+import requireLogin from '#modules/gestor/app/middlewares/requireLogin.js';
 import { listarBancos } from '#modules/gestor/app/controllers/bancoApiController.js';
 const router = express.Router();
 
-router.get('/bancos', listarBancos);
+router.get('/bancos', requireLogin, listarBancos);
 
 export default router;
